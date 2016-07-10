a36b5a2f2d0d98db1d060e2ead10ba0b6c86aec7
Esther Leytush <eleytush@gmail.com>
2016-07-10 08:06:37 -0400

    remove whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ActiveRecord::Base
   has_one :latest_post, -> { order(created_at: :desc).limit(1) }, class_name: "Post"
 
   validates :username, presence: true
-
   has_secure_password
-
 end
