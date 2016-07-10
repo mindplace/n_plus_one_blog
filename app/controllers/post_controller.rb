6efa0cabb35c6be7beed9a51631f155294aaac34
Esther Leytush <eleytush@gmail.com>
2016-07-09 23:30:17 -0400

    clean up layout and posts

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,6 @@
 class PostController < ApplicationController
   def index
-    @posts = Post.order(created_at: :desc).limit(5)
+    @posts = Post.order(created_at: :desc).limit(10)
   end
 
   def new
